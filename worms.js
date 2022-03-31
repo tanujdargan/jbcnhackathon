fcaea91ad28eca68ace089711ed421555aa29845
tanuj_dargan <dargantanuj@gmail.com>
2022-03-31 17:04:44 +0530

    scrolling 4k px page, fixed lottie animations and text

--- worms.js
+++ worms.js
@@ -1,3 +1,8 @@
+let scrollHeight = Math.max(
+  document.body.scrollHeight, document.documentElement.scrollHeight,
+  document.body.offsetHeight, document.documentElement.offsetHeight,
+  document.body.clientHeight, document.documentElement.clientHeight
+);
 /*--------------------
 Vars
 --------------------*/
